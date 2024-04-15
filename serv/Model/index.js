25f31f80f750e3c6f537c673bfcaf57ea6cb6e7b
RamySassi <rami9b3@live.fr>
2024-04-15 19:29:02 +0200

    y

--- serv/Model/index.js
+++ serv/Model/index.js
@@ -1,6 +1,8 @@
-const  { PrismaClient } =require('@prisma/client') 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+
+
 
 async function main() {
   const user = await prisma.user.findMany()
