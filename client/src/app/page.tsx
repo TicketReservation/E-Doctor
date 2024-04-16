8c45c90407e4f039ef63b2c50161944c78d138db
mahdi <gatrimahdi5@gmail.com>
2024-04-16 11:56:56 +0100

    toRami

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import DoctorProfilePage from "./components/DoctorProfile/DoctorProfilePage";
 
 export default function Home() {
-  return (<h1>hello</h1>)
-  }
+  return (
+    <DoctorProfilePage/>
+  )
+}
