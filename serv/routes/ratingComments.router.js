379d288e5ada13433134edc6f4235699d603304f
RamySassi <rami9b3@live.fr>
2024-04-17 11:10:11 +0200

    back

--- serv/routes/ratingComments.router.js
+++ serv/routes/ratingComments.router.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const RatingsComments = require('../controllers/ratingComments.controller');
 
 router.get('/user/:userId', RatingsComments.getReviewsByUserId);
-router.post('/add', RatingsComments.addReview);
 
 router.get('/doctor/:doctorId', RatingsComments.getReviewsByDoctorId);
 router.get('/all', RatingsComments.getAll);
 
+router.post('/add', RatingsComments.addReview);
 
 
 
