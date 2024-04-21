a4879a891aa058aeb0cde22ff2b6f7624e66f39d
Sassi Ramy <154595189+RamySassi@users.noreply.github.com>
2024-04-21 21:44:05 +0200

    Delete serv directory

--- serv/routes/userrouters.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const usercontroller = require("../controllers/usercontroller")
-
-router.get('/all', usercontroller.getAllUsers)
-router.get('/:id', usercontroller.getUserById)
-router.put('/update/:id', usercontroller.updateUser)
-router.delete('/delete/:Email', usercontroller.deleteUser)
-
-module.exports = router;
